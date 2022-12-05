be75e0d73e618a6e633114f9e361b5373f6e5a9e
Mohannad Abu Ajameih <Mohannad Abu Ajameih@MOHANNAD>
2022-12-05 21:45:08 +0300

    Add project files.

---
+++ WebApplication1/Models/EmpContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Models
+{
+    public class EmpContext : DbContext
+    {
+
+        public EmpContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Department> Departments { get; set; }
+
+    }
+}
